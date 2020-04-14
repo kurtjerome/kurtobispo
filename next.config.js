95d28a98254b59321ab98c8131e1d13eabf87353
Kurt Obispo <kurtobispo@gmail.com>
2020-04-14 15:02:42 +0800

    Fix enivronment variable name

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
     contentfulSpaceId: process.env.CONTENTFUL_SPACE_ID,
-    contentfulAccessToken: process.env.CONTENTFUL_ACESS_TOKEN,
+    contentfulAccessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   },
 }
